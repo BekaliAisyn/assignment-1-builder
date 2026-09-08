887639f346c80a822e5cad80706532aed0ddf90e
bekali <ajsynbekali@gmail.com>
2026-09-08 13:46:59 +0500

    initial domain model

---
+++ src/Computer.java
@@ -0,0 +1,35 @@
+public class Computer {
+
+    String cpu;
+    int ram;
+    int storage;
+    String operatingSystem;
+    String gpu;
+    double screenSize;
+    boolean wifi;
+    boolean bluetooth;
+    boolean keyboard;
+    boolean mouse;
+    boolean gamingMode;
+    int warrantyYears;
+    Monitor monitor;
+
+    public Computer(String cpu, int ram, int storage, String operatingSystem, String gpu,
+                    double screenSize, boolean wifi, boolean bluetooth, boolean mouse, boolean keyboard,
+                    boolean gamingMode, int warrantyYears, Monitor monitor) {
+        this.cpu = cpu;
+        this.ram = ram;
+        this.storage = storage;
+        this.operatingSystem = operatingSystem;
+        this.gpu = gpu;
+        this.screenSize = screenSize;
+        this.wifi = wifi;
+        this.bluetooth = bluetooth;
+        this.keyboard = keyboard;
+        this.mouse = mouse;
+        this.gamingMode = gamingMode;
+        this.warrantyYears = warrantyYears;
+        this.monitor = monitor;
+    }
+
+}
---
+++ src/Monitor.java
@@ -0,0 +1,12 @@
+public class Monitor {
+    String brand;
+    double size;
+    String resolution;
+
+    public Monitor(String brand, double size, String resolution){
+        this.brand = brand;
+        this.size = size;
+        this.resolution = resolution;
+    }
+
+}
